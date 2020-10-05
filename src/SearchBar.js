f4df118d4a772526f1b9e7c0c3a60cc90a600a43
Jon Washington <jonathan.washington@gmail.com>
2020-10-04 17:28:15 -0700

    Initial commit, testing efficacy of gitignore

---
+++ src/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class searchBar extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(<h1>"Hey it worked!"</h1>);
+  }
+}
+
+export default searchBar;
